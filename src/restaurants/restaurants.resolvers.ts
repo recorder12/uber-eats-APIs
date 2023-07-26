938fe7fcafc2205ecb2ad6dd67c3ad006e0187fc
Byeonggeon Kim <spinkbg@gmail.com>
2023-07-26 17:36:33 -0400

    finish GraphSQL section

--- src/restaurants/restaurants.resolvers.ts
+++ src/restaurants/restaurants.resolvers.ts
@@ -1,9 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurants.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver()
 export class RestaurantsResolver {
-    @Query((returns) => Boolean)
-    isPizzaGood(): boolean {
+    @Query((returns) => [Restaurant]) // GraphQL type
+    restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+        return []
+    }
+
+    @Mutation((returns) => Boolean)
+    createRestaurant(
+        @Args() createRestaurantInput: CreateRestaurantDto
+    ): boolean {
+        console.log(createRestaurantInput);
         return true;
     }
 }
